eec49796a509385c4ceed197938362c44817f98d
DFK-Z <guga-2005av@mail.ru>
2025-12-11 16:16:10 +0300

    check

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { TasksModule } from './tasks/tasks.module';
 import { CategoriesModule } from './categories/categories.module';
 import { EquipmentsModule } from './equipments/equipments.module';
-import { RentalsModule } from './rentals/rentals.module';
 import { PaymentsModule } from './payments/payments.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, TasksModule, CategoriesModule, EquipmentsModule, RentalsModule, PaymentsModule, UsersModule],
+  imports: [PrismaModule, AuthModule, CategoriesModule, EquipmentsModule, PaymentsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
